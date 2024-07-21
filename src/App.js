5a1173fb9a72718e35133cd383d96d03c95e4246
Razvan Muntean <munteanr20@gmail.com>
2024-07-21 15:32:30 +0300

    updated router

--- src/App.js
+++ src/App.js
@@ -6,25 +6,27 @@ import FAQ from './components/FAQ/FAQ';
 import CoursePage from './components/CoursePage/CoursePage';
 import Contact from './components/Contact/Contact';
 import Home from './components/Home/Home';
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, Router } from "react-router-dom";
 import { QuestionProvider } from "./components/Application/QuestionContext";
 
 function App() {
   return (
     <>
-      <QuestionProvider>
-        <Navbar />
-        <div className="container">
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/application" element={<Application />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/faq" element={<FAQ />} />
-            <Route path="/contact" element={<Contact />} />
-            <Route path="/course/:cod" element={<CoursePage />} />
-          </Routes>
-        </div>
-      </QuestionProvider>
+      <Router>
+        <QuestionProvider>
+          <Navbar />
+          <div className="container">
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/application" element={<Application />} />
+              <Route path="/about" element={<About />} />
+              <Route path="/faq" element={<FAQ />} />
+              <Route path="/contact" element={<Contact />} />
+              <Route path="/course/:cod" element={<CoursePage />} />
+            </Routes>
+          </div>
+        </QuestionProvider>
+      </Router>
     </>
   );
 }
